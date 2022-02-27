40151b6bbac649b48770ab73f7b993ede99e8bfa
Danny <93397389+Dcastro99@users.noreply.github.com>
2022-02-26 16:07:33 -0800

    added starter codes in react

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <>
+        <h3>Created by: Danny C.</h3>
+      </>
+    )
+  }
+}
+
+export default Footer;
---
+++ src/HornedBeast.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class HornedBeast extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>{this.props.title}</h2>
+        <img src="" alt="" title=""></img>
+        <p>{this.props.description}</p>
+      </div>
+      
+      
+    )
+  }
+}
+
+export default HornedBeast;
