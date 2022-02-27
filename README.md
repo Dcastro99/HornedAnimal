0e57c717496bf1c2a83d3f76e8e204feee021028
Danny <93397389+Dcastro99@users.noreply.github.com>
2022-02-26 18:00:27 -0800

    finished lab 01

---
+++ README.md
@@ -0,0 +1,5 @@
+# References
+
+- <https://a-z-animals.com/blog/8-animals-with-one-horn/>
+
+- <https://en.wikipedia.org/wiki>
--- src/Main.js
+++ src/Main.js
@@ -5,7 +5,12 @@ class Main extends React.Component {
   render() {
     return (
       <>
-       <HornedBeast title={'hello'} description={'goodbye!'}/>
+       <HornedBeast 
+
+       title1={'Horned Gopher'} description1={'Horned gophers are the smallest known horned mammals and the only known rodents ever to have had horns.'}
+       title2={'Cassowary'} description2={'It\'s horn is called a casque. It has an inner layer of soft, spongy material and an outer layer of keratin. If you were to squeeze its horn, it would be soft in its middle portion.'}
+       title3={'Nubian Ibex'} description3={'Desert-dwelling goat species found in mountainous areas of northern and northeast Africa, and the Middle East.'}
+       />
       </>
     )
   }
