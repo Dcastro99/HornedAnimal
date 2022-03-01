0a494bd87aed3ea06795fc11ba3131fc31d256f7
Danny <93397389+Dcastro99@users.noreply.github.com>
2022-02-28 20:41:50 -0800

    fixed prop code

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,9 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
+import HornedGopher from "./horned-gophers.jpeg"
+import Cassowary from "./cassowary1.jpg"
+import NubianIbex from "./nubianibex.jpeg"
+
 
 class Main extends React.Component {
   render() {
@@ -7,10 +11,17 @@ class Main extends React.Component {
       <>
        <HornedBeast 
 
-       title1={'Horned Gopher'} description1={'Horned gophers are the smallest known horned mammals and the only known rodents ever to have had horns.'}
-       title2={'Cassowary'} description2={'It\'s horn is called a casque. It has an inner layer of soft, spongy material and an outer layer of keratin. If you were to squeeze its horn, it would be soft in its middle portion.'}
-       title3={'Nubian Ibex'} description3={'Desert-dwelling goat species found in mountainous areas of northern and northeast Africa, and the Middle East.'}
-       />
+       title={'Horned Gopher'} imageUrl={HornedGopher} alt={'Horned Gopher Skeleton'} description={'Horned gophers are the smallest known horned mammals and the only known rodents ever to have had horns.'}/>
+
+       <HornedBeast 
+
+       title={'Cassowary'} imageUrl={Cassowary} alt={'Horned Bird'} description={'It\'s horn is called a casque. It has an inner layer of soft, spongy material and an outer layer of keratin. If you were to squeeze its horn, it would be soft in its middle portion.'}/>
+
+       <HornedBeast 
+
+       title={'Nubian Ibex'} imageUrl={NubianIbex} alt={'Horned Animal'} description={'Desert-dwelling goat species found in mountainous areas of northern and northeast Africa, and the Middle East.'}/>
+      
+       
       </>
     )
   }
