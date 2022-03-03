e7ec90ec06363db9b653e1e64992cff2cfca61d0
Danny <93397389+Dcastro99@users.noreply.github.com>
2022-03-02 21:40:23 -0800

    removed code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
-import Main from './Main';
-import Footer from './Footer';
-import {Container, Row, Col} from 'react-bootstrap'
-
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
 
 class App extends React.Component {
   render() {
-
-  
     return (
-      
       <div className="App">
-      
-      <Header/>
-      <Main/>
-      <Footer/>
+        <Header />
+        <Main />
+        <Footer />
       </div>
     );
   }
 }
-
 
 export default App;
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,34 +1,36 @@
 import React from "react";
-import Card from 'react-bootstrap/Card'
+import Card from "react-bootstrap/Card";
 
 class HornedBeast extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      favorited: 0 
-    }
+      favorited: 0,
+    };
   }
 
   favorites = () => {
     this.setState({
-      favorited: this.state.favorited + 1
-    })
-  }
+      favorited: this.state.favorited + 1,
+    });
+  };
 
   render() {
     return (
-      
-      <Card className="animalCard" style={{ width: '18rem' }}>
+      <Card className="animalCard" style={{ width: "18rem" }}>
         <Card.Title>{this.props.name}</Card.Title>
-          <Card.Img variant="top" src={this.props.img} alt={this.props.alt} onClick={this.favorites}/>
-            <Card.Body>
-              <Card.Text>favorited ={this.state.favorited} ❤️</Card.Text>
-            </Card.Body>
+        <Card.Img
+          variant="top"
+          src={this.props.img}
+          alt={this.props.alt}
+          onClick={this.favorites}
+        />
+        <Card.Body>
+          <Card.Text>favorited ={this.state.favorited} ❤️</Card.Text>
+        </Card.Body>
       </Card>
-    
-    )
+    );
   }
 }
 
 export default HornedBeast;
-
